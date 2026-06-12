96a4e399dae7366636d430331c095fc82753f552
gopnikls <yanko.yan@seznam.cz>
2026-06-12 12:51:22 +0200

    Add ModelMapper dependency and bean

--- src/main/java/com/yankoyan/spring/bank_api/config/ApplicationConfiguration.java
+++ src/main/java/com/yankoyan/spring/bank_api/config/ApplicationConfiguration.java
@@ -1,7 +1,13 @@
 package com.yankoyan.spring.bank_api.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
